6e2ad8fc75a629b3d025fbaee5c518014538d72e
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2023-11-20 14:56:47 +0100

    data and seo

--- components/partials/sections/home/FirstSection.vue
+++ components/partials/sections/home/FirstSection.vue
@@ -18,23 +18,28 @@
                     </button>
                 </div>
 
-                <div class=" w-full flex flex-wrap sm:flex-nowrap gap-4 sm:gap-0 justify-center items-center  sm:justify-between max-w-[35rem] mx-auto">
+                <div class=" w-full flex flex-wrap sm:flex-nowrap gap-4  justify-center items-center  sm:justify-between max-w-[35rem] mx-auto">
                     <div class=" flex items-center space-x-3">
-                        <label for="style" class=" text-black text-[0.9375rem] font-Inter font-normal">🎨 Style</label>
-                        <select name="style" id="style" class="pl-2 capitalize border-2 rounded-[0.625rem] border-black px-1 py-1 text-[0.9375rem] font-Inter font-normal">
-                            <option value="cinematic">cinematic</option>
+                        <label for="style" class=" text-black text-[0.9375rem] font-Inter font-normal whitespace-nowrap">🎨 Style</label>
+                        <select name="style" id="style" class="pl-2 w-[7.3125rem] capitalize border-2 rounded-[0.625rem] border-black px-1 py-1 text-[0.9375rem] font-Inter font-normal">
+                            <option v-for="(style,index,key) in Styles" :key="key" :value="style.value">{{style.name}}</option>
                         </select>
                     </div>
                      <div class=" flex items-center space-x-3">
                         <label for="style" class=" text-black text-[0.9375rem] font-Inter font-normal">Shape</label>
                         <select name="style" id="style" class=" pl-2 capitalize border-2 rounded-[0.625rem] border-black px-1 py-1 text-[0.9375rem] font-Inter font-normal">
-                            <option value="shape">shape</option>
+                            <option value="portrait">Portrait</option>
+                            <option value="square">Square</option>
+                            <option value="landscape">Landscape</option>
                         </select>
                     </div>
                      <div class=" flex items-center space-x-3">
-                        <label for="style" class=" text-black text-[0.9375rem] font-Inter font-normal">Number of images</label>
+                        <label for="style" class=" text-black text-[0.9375rem] font-Inter font-normal whitespace-nowrap">Number of images</label>
                         <select name="style" id="style" class=" pl-4 capitalize border-2 rounded-[0.625rem] border-black px-1 py-1 text-[0.9375rem] font-Inter font-normal">
                             <option value="1">1</option>
+                            <option value="3">3</option>
+                            <option value="6">6</option>
+                            
                         </select>
                     </div>
                 </div>
@@ -51,6 +56,305 @@
 
 <script setup>
  import ImageCard from '~/components/UI/ImageCard.vue'
+
+ const Styles = ref([
+     {
+        name:'Cinematic',
+        value:'cinematic',
+    },
+    {
+        name:'Painted Anime',
+        value:'painted anime',
+    },
+    {
+        name:'Casual Photo',
+        value:'casual photo',
+    },
+    {
+        name:'Digital Painting',
+        value:'digital painting',
+    },
+    {
+        name:'Concept Art',
+        value:'concept art',
+    },
+    {
+        name:'𝗡𝗼 𝘀𝘁𝘆𝗹𝗲',
+        value:'no style',
+    },
+    {
+        name:'3D Disney Character',
+        value:'3d disney character',
+    },
+    {
+        name:'2D Disney Character',
+        value:'2d disney character',
+    },
+    {
+        name:'Disney Sketch',
+        value:'disney sketch',
+    },
+    {
+        name:'Concept Sketch',
+        value:'concept sketch',
+    },
+    {
+        name:'Painterly',
+        value:'painterly',
+    },
+    {
+        name:'Oil Painting',
+        value:'oil painting',
+    },
+    {
+        name:'Oil Painting - Realism',
+        value:'oil painting - realism',
+    },
+    {
+        name:'Oil Painting - Old',
+        value:'oil painting - old',
+    },
+    {
+        name:'Professional Photo',
+        value:'professional photo',
+    },
+    {
+        name:' Anime',
+        value:' anime',
+    },
+    {
+        name:'Cute Anime',
+        value:'cute anime',
+    },
+    {
+        name:'Soft Anime',
+        value:'soft anime',
+    },
+    {
+        name:'Fantasy Painting',
+        value:'fantasy painting',
+    },
+    {
+        name:'Fantasy Landscape',
+        value:'fantasy landscape',
+    },
+    {
+        name:'Fantasy Portrait',
+        value:'fantasy portrait',
+    },
+    {
+        name:'Studio Ghibli',
+        value:'studio ghibli',
+    },
+    {
+        name:'50s Enamel Sign',
+        value:'50s enamel sign',
+    },
+    {
+        name:'Vintage Comic',
+        value:'vintage comic',
+    },
+    {
+        name:'Franco-Belgian Comic',
+        value:'franco-belgian comic',
+    },
+    {
+        name:'Tintin Comic',
+        value:'tintin comic',
+    },
+    {
+        name:'Medieval',
+        value:'medieval',
+    },
+    {
+        name:'Pixel Art',
+        value:'pixel art',
+    },
+    {
+        name:'Cute Figurine',
+        value:'cute figurine',
+    },
+    {
+        name:'3D Emoji',
+        value:'3d emoji',
+    },
+    {
+        name:'Illustration',
+        value:'illustration',
+    },
+    {
+        name:'Flat Illustration',
+        value:'flat illustration',
+    },
+    {
+        name:'Watercolor',
+        value:'watercolor',
+    },
+    {
+        name:'1990s Photo',
+        value:'1990s photo',
+    },
+    {
+        name:'1980s Photo',
+        value:'1980s photo',
+    },
+    {
+        name:'1970s Photo',
+        value:'1970s photo',
+    },
+    {
+        name:'1960s Photo',
+        value:'1960s photo',
+    },
+    {
+        name:'1950s Photo',
+        value:'1950s photo',
+    },
+    {
+        name:'1940s Photo',
+        value:'1940s photo',
+    },
+    {
+        name:'1930s Photo',
+        value:'1930s photo',
+    },
+    {
+        name:'1920s Photo',
+        value:'1920s photo',
+    },
+    {
+        name:'Vintage Pulp Art',
+        value:'vintage pulp art',
+    },
+    {
+        name:'50s Infomercial Anime',
+        value:'50s infomercial anime',
+    },
+    {
+        name:'3D Pokemon',
+        value:'3d pokemon',
+    },
+    {
+        name:'Painted Pokemon',
+        value:'painted pokemon',
+    },
+    {
+        name:'2D Pokemon',
+        value:'2d pokemon',
+    },
+    {
+        name:'Vintage Anime',
+        value:'vintage anime',
+    },
+    {
+        name:'Neon Vintage Anime',
+        value:'neon vintage anime',
+    },
+    {
+        name:'Manga',
+        value:'manga',
+    },
+    {
+        name:'Fantasy World Map',
+        value:'fantasy world map',
+    },
+    {
+        name:'Fantasy City Map',
+        value:'fantasy city map',
+    },
+    {
+        name:'Old World Map',
+        value:'old world map',
+    },
+    {
+        name:'3D Isometric Icon',
+        value:'3d isometric icon',
+    },
+    {
+        name:'Flat Style Icon',
+        value:'flat style icon',
+    },
+    {
+        name:'Flat Style Logo',
+        value:'flat style logo',
+    },
+    {
+        name:'Game Art Icon',
+        value:'game art icon',
+    },
+    {
+        name:'Digital Painting Icon',
+        value:'digital painting icon',
+    },
+    {
+        name:'Concept Art Icon',
+        value:'concept art icon',
+    },
+    {
+        name:'Cute 3D Icon',
+        value:'cute 3d icon',
+    },
+    {
+        name:'Cute 3D Icon 𝗦𝗲𝘁',
+        value:'cute 3d icon set',
+    },
+    {
+        name:'Crayon Drawing',
+        value:'crayon drawing',
+    },
+    {
+        name:'Pencil',
+        value:'pencil',
+    },
+    {
+        name:'Tattoo Design',
+        value:'tattoo design',
+    },
+    {
+        name:'Waifu',
+        value:'waifu',
+    },
+    {
+        name:'YuGiOh Art',
+        value:'yugioh art',
+    },
+    {
+        name:'Traditional Japanese',
+        value:'traditional japanese',
+    },
+    {
+        name:'Nihonga Painting',
+        value:'nihonga painting',
+    },
+    {
+        name:'Claymation',
+        value:'claymation',
+    },
+    {
+        name:'Furry - Painted',
+        value:'furry - painted',
+    },
+    {
+        name:'Furry - Drawn',
+        value:'furry - drawn',
+    },
+    {
+        name:'Furry - Cinematic',
+        value:'furry - cinematic',
+    },
+    {
+        name:'Cartoon',
+        value:'cartoon',
+    },
+    {
+        name:'Cursed Photo',
+        value:'cursed photo',
+    },
+    {
+        name:'MTG Card',
+        value:'mtg card',
+    },
+ ])
 </script>
 
 
